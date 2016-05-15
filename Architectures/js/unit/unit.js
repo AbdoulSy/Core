4b20beff7959330fb9d27ef7b2e6c08a955a415b
Abdoul Sy <dreescan@gmail.com>
2016-05-15 19:08:18 +0100

    updated unit.js with its oldest tutorial as a reference

--- Architectures/js/unit/unit.js
+++ Architectures/js/unit/unit.js
@@ -2,10 +2,13 @@ class Unit {
   constructor(name = 'Unit') {
     this.name = name;
   }
-  getCodeStyleUrl(ECMAScriptVersion = 6) {
+  getCodeStyleUrl (ECMAScriptVersion = 6) {
     if (ECMASCriptVersion === 6) {
       return 'https://github.com/airbnb/javascript';
     }
+  }
+  getOldestES6SetupTutorialUrl () {
+    return 'http://codeutopia.net/blog/2014/12/18/getting-started-with-using-es6-ecmascript-6/'
   }
 }
 
